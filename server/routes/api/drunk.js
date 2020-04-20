b528f23c64b8e5c52b884f96550b898aa4ba9d7d
dannmate <dpetrolito@gmail.com>
2020-04-19 18:53:44 -0700

    added confirm route and mongodb

--- server/routes/api/drunk.js
+++ server/routes/api/drunk.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const mongodb  = require('mongodb');
+require('dotenv').config();
 
 const router = express.Router();
 
-// Get if my mate is drunk or notbundleRenderer.renderToStream
+// Get if my mate is drunk or not
+router.get('/', async (req,res) => {
+    const confirmations = await getConfirmations();
 
-router.get('/', (req,res) => {
-    res.send('Hello');
+    res.send(await confirmations.find({}).toArray());
 });
+
+router.post('/confirm', async (req,res) => {
+    const confirmations = await getConfirmations();
+
+    await confirmations.insertOne({
+        user: req.body.user,
+        sumbittedAt: new Date(),
+        image: ''
+
+    });
+    res.status(201).send();
+    res.send(await confirmations.find({}).toArray());
+});
+
+async function getConfirmations() {
+    console.log(process.env);
+    const client = await mongodb.MongoClient.connect(
+        process.env.MONGODB_CONNECTION_STRING,{useNewUrlParser: true, useUnifiedTopology: true});
+
+    return client.db('isxdrunk').collection('confirmation');
+
+}
 
 module.exports = router;
